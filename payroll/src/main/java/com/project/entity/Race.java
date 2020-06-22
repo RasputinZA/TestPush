cf81863cd745a84b4dd6767da6a39fd96846a975
RasputinZA <RasputinZA1357@users.noreply.github.com>
2020-06-22 16:04:44 +0200

    First Commit
    First Commit

---
+++ payroll/src/main/java/com/project/entity/Race.java
@@ -0,0 +1,48 @@
+package com.project.entity;
+
+public class Race {
+
+    private String id, desc;
+
+    private Race(Builder builder)
+    {
+        this.id = builder.id;
+        this.desc = builder.desc;
+    }
+
+    @Override
+    public String toString() {
+        return "Race{" +
+                "id='" + id + '\'' +
+                ", desc='" + desc + '\'' +
+                '}';
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getDesc() {
+        return desc;
+    }
+
+    public static class Builder {
+        private String id, desc;
+
+        public Builder setId(String id) {
+            this.id = id;
+            return this;
+        }
+
+        public Builder setDesc(String desc) {
+            this.desc = desc;
+            return this;
+        }
+
+        public Builder copy(Race race) {
+            this.id = race.id;
+            this.desc = race.desc;
+            return this;
+        }
+    }
+}
